21eb82efd33e62b2749e9cb0dd95e7a2ac718095
anees199 <anees4865577@gmail.com>
2025-02-10 00:08:53 -0800

    login register to home page done

---
+++ scripts/firebaseconfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAXSp14_7uiH6jBC5qwCcTMTash2yVTt7c",
+    authDomain: "login-example-18fa5.firebaseapp.com",
+    projectId: "login-example-18fa5",
+    storageBucket: "login-example-18fa5.firebasestorage.app",
+    messagingSenderId: "537848456792",
+    appId: "1:537848456792:web:b3716e1f01c6762276cc2d",
+    measurementId: "G-8GKDW9GG2B"
+  };
+
+  export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
